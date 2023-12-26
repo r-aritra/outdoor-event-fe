824b98548c52b799a325afc3680f99b5dcbfa83f
rutvik balar <76244286+rutvik-balar@users.noreply.github.com>
2023-12-26 14:30:57 +0530

    add protected routes

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
 import BatchEventApprove from '../features/BatchEventApprove';
 import BatchEventRegister from '../features/BatchEventRegister';
@@ -8,16 +14,25 @@ import Home from '../features/Home';
 import Login from '../features/Login';
 import Signup from '../features/Signup';
 
+const isAuthenticated = false;
+
+const PrivateRoutes = () => {
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
+
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/batch-event/results" element={<BatchEventApprove />} />
+          <Route path="/batch-event/register" element={<BatchEventRegister />} />
+          <Route path="/batch-event/approve" element={<BatchEventResults />} />
+        </Route>
+
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/batch-event/results" element={<BatchEventResults />} />
-        <Route path="/batch-event/register" element={<BatchEventRegister />} />
-        <Route path="/batch-event/approve" element={<BatchEventApprove />} />
       </Routes>
     </Router>
   );

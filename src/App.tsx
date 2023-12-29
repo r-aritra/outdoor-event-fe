f232d98c5d37feb2786b9aec0be4e02d0358ea3b
rutvik balar <76244286+rutvik-balar@users.noreply.github.com>
2023-12-29 12:40:46 +0530

    remove notification

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
 
 import { MantineProvider } from '@mantine/core';
-import { Notifications } from '@mantine/notifications';
-import React from 'react';
 
 import AppRoutes from './router/AppRoutes';
 
 function App() {
   return (
     <MantineProvider>
-      <Notifications />
       <AppRoutes />
     </MantineProvider>
   );

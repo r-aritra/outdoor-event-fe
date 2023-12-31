f4c40a49a1613c795778358c5ed69e67bdb18bcf
rutvik balar <76244286+rutvik-balar@users.noreply.github.com>
2023-12-31 22:44:26 +0530

    add in both page

--- src/config/translations/locales/en.ts
+++ src/config/translations/locales/en.ts
@@ -1,3 +1,35 @@
+// en.js
+
 export const en = {
-  home: 'welcome to home page',
+  page: {
+    login: {
+      welcome: 'Welcome to booking.com',
+      emailLabel: 'Email',
+      emailPlaceholder: 'hello@mantine.dev',
+      invalidEmailError: 'Invalid email',
+      passwordLabel: 'Password',
+      passwordPlaceholder: 'Your password',
+      invalidPasswordError: 'Password should include at least 6 characters',
+      registerPrompt: "Don't have an account? Register",
+      loginButton: 'Login',
+    },
+    signup: {
+      welcome: 'Welcome to booking.com',
+      nameLabel: 'Name',
+      namePlaceholder: 'Your name',
+      emailLabel: 'Email',
+      emailPlaceholder: 'hello@mantine.dev',
+      invalidEmailError: 'Invalid email',
+      passwordLabel: 'Password',
+      passwordPlaceholder: 'Your password',
+      invalidPasswordError: 'Password should include at least 6 characters',
+      vendorRegisterLabel: 'Register as a vendor',
+      otpLabel: 'Enter OTP',
+      otpPlaceholder: '123456',
+      invalidOTPError: 'Invalid OTP',
+      loginPrompt: 'Already have an account? Login',
+      validateOTPButton: 'Validate OTP',
+      registerButton: 'Register',
+    },
+  },
 };
